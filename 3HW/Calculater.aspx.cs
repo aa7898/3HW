c9812e89adec9ab8287ba8217018f09dea10bbcc
奕淳 王 <forever2635@yahoo.com>
2021-03-30 16:42:52 +0800

    做完1~9、加減乘除

--- 3HW/Calculater.aspx.cs
+++ 3HW/Calculater.aspx.cs
@@ -29,12 +29,13 @@ namespace _3HW {
             }
 
             int i_Ind = (o_Num.i_BoundCount - 1);
-            i_Num = (o_Num.ia_List)[i_Ind];
+            i_Num = o_Num.ia_List[i_Ind];
             lb_Result.Text = i_Num.ToString();
         }
 
         protected void bt_Add_Click(object sender, EventArgs e) {
-            if (o_Num.i_BoundCount > 1) {
+            if (o_Num.i_BoundCount > 1)
+            {
                 o_Num.ia_List[0] += o_Num.ia_List[1];
                 o_Num.ia_List[1] = 0;
                 o_Num.i_BoundCount--;
@@ -47,15 +48,45 @@ namespace _3HW {
         }
 
         protected void bt_Miuns_Click(object sender, EventArgs e) {
-
+            if (o_Num.i_BoundCount > 1)
+            {
+                o_Num.ia_List[0] -= o_Num.ia_List[1];
+                o_Num.ia_List[1] = 0;
+                o_Num.i_BoundCount--;
+            }
+            o_Num.i_Operater = 2;
+            lb_Operate.Text = "-";
+            o_Num.ia_List[1] = 0;
+            o_Num.i_BoundCount++;
+            ViewState["PreivousInfo"] = o_Num;
         }
 
         protected void bt_Product_Click(object sender, EventArgs e) {
-
+            if (o_Num.i_BoundCount > 1)
+            {
+                o_Num.ia_List[0] *= o_Num.ia_List[1];
+                o_Num.ia_List[1] = 0;
+                o_Num.i_BoundCount--;
+            }
+            o_Num.i_Operater = 3;
+            lb_Operate.Text = "*";
+            o_Num.ia_List[1] = 0;
+            o_Num.i_BoundCount++;
+            ViewState["PreivousInfo"] = o_Num;
         }
 
         protected void bt_Divide_Click(object sender, EventArgs e) {
-
+            if (o_Num.i_BoundCount > 1)
+            {
+                o_Num.ia_List[0] /= o_Num.ia_List[1];
+                o_Num.ia_List[1] = 0;
+                o_Num.i_BoundCount--;
+            }
+            o_Num.i_Operater = 4;
+            lb_Operate.Text = "/";
+            o_Num.ia_List[1] = 0;
+            o_Num.i_BoundCount++;
+            ViewState["PreivousInfo"] = o_Num;
         }
 
         protected void bt_Clear_Click(object sender, EventArgs e) {
@@ -71,6 +102,30 @@ namespace _3HW {
                         o_Num.ia_List[0] += o_Num.ia_List[1];
                         o_Num.ia_List[1] = 0;
                         o_Num.i_BoundCount--;                        
+                    }            
+                    break;
+                case 2:
+                    if (o_Num.i_BoundCount > 1)
+                    {
+                        o_Num.ia_List[0] -= o_Num.ia_List[1];
+                        o_Num.ia_List[1] = 0;
+                        o_Num.i_BoundCount--;
+                    }
+                    break;
+                case 3:
+                    if (o_Num.i_BoundCount > 1)
+                    {
+                        o_Num.ia_List[0] *= o_Num.ia_List[1];
+                        o_Num.ia_List[1] = 0;
+                        o_Num.i_BoundCount--;
+                    }
+                    break;
+                case 4:
+                    if (o_Num.i_BoundCount > 1)
+                    {
+                        o_Num.ia_List[0] /= o_Num.ia_List[1];
+                        o_Num.ia_List[1] = 0;
+                        o_Num.i_BoundCount--;
                     }
                     break;
                 default:
@@ -113,31 +168,94 @@ namespace _3HW {
         }
 
         protected void bt_3_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "3";
+            }
+            else
+            {
+                lb_Result.Text += "3";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_4_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "4";
+            }
+            else
+            {
+                lb_Result.Text += "4";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_5_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "5";
+            }
+            else
+            {
+                lb_Result.Text += "5";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_6_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "6";
+            }
+            else
+            {
+                lb_Result.Text += "6";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_7_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "7";
+            }
+            else
+            {
+                lb_Result.Text += "7";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_8_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "8";
+            }
+            else
+            {
+                lb_Result.Text += "8";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         protected void bt_9_Click(object sender, EventArgs e) {
-
+            if (Convert.ToInt32(lb_Result.Text) == 0)
+            {
+                lb_Result.Text = "9";
+            }
+            else
+            {
+                lb_Result.Text += "9";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
         }
 
         private void mt_SetInfo() {
